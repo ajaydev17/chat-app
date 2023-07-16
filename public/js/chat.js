dd4a67fd6a864bb3cc3cb2dc41d9916d440df26f
Ajay Dev <devadigaajay1729@gmail.com>
2023-07-16 16:57:00 +0530

    Exploration of socket io

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,13 @@
-io();
+const socket = io();
+
+// listen to event from the server
+socket.on('countUpdated', (count) => {
+    console.log("The value of the count has been updated!!", count);
+});
+
+// listen on click event on the button
+document.querySelector("#increment").addEventListener('click', (event) => {
+    // update the count and send back to server
+    console.log("clicked");
+    socket.emit('increment');
+});
--- src/index.js
+++ src/index.js
@@ -12,8 +12,24 @@ const port = process.env.PORT || 3000;
 // serve static files from the public directory
 app.use(express.static(path.join(__dirname, '../public')));
 
+let count = 0;
+
 io.on('connection', (socket) => {
     console.log("New websocket connection");
+
+    // emit an action on new connection to client
+    socket.emit('countUpdated', count);
+
+    // listen to emit action from the client
+    socket.on('increment', () => {
+        count = count + 1;
+
+        // will update only the client which performed click operation
+        // socket.emit('countUpdated', count);
+
+        // will update all the clients
+        io.emit('countUpdated', count);
+    });
 });
 
 server.listen(port, () => {
