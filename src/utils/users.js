8881b9ea41387aa2ba8af13c619fb21065bc71a2
Ajay Dev <devadigaajay1729@gmail.com>
2023-07-30 23:44:37 +0530

    logic to check user already exists in the chat room

--- src/utils/users.js
+++ src/utils/users.js
@@ -34,3 +34,19 @@ const removeUser = (id) => {
         return users.splice(index, 1)[0];
     }
 };
+
+const getUser = (id) => {
+    return users.find((user) => user.id === id);
+};
+
+const getUsersInRoom = (roomname) => {
+    room = room.trim().toLowerCase();
+    return users.filter((user) => user.roomname === roomname);
+};
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom,
+};
